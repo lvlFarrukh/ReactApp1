c87412cab70fa61184a2effd635871f33af9b034
Farrukh Aslam <farrukha303@gmail.com>
2020-07-27 13:16:32 -0700

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Apps from './App';
-import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
